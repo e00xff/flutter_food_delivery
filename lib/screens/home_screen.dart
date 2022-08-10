5c8c10454f35079bf266d381e6e5c041ad5ce3e3
Levan <e00xff@gmail.com>
2022-08-10 22:59:25 +0400

    search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -26,7 +27,38 @@ class _HomeScreenState extends State<HomeScreen> {
               textStyle: const TextStyle(fontSize: 20),
             ),
             onPressed: () {},
-            child: const Text('Cart (5)'),
+            child: Text('Cart (${currentUser.cart.length})'),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: const BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8, color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search food or restaurant',
+                prefixIcon: const Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {},
+                ),
+              ),
+            ),
           ),
         ],
       ),
